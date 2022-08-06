4dfbeb5353b7b6294c84914cccfe1f55de64151b
Bruno Henrique <brunohosilva97@gmail.com>
2022-08-06 12:15:57 -0300

    :sparkles: new feature

--- app/src/main/java/com/example/roletadasorte/MainActivity.kt
+++ app/src/main/java/com/example/roletadasorte/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.roletadasorte
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
@@ -14,15 +16,36 @@ class MainActivity : AppCompatActivity() {
         
         // Declarado variavel para resgatar o objeto do botão na view     
         val btnRodarRoleta = findViewById<Button>(R.id.btn_rodar)
-        
         // Acessa a propriedade on click do botão
         // onde é chadado o metodo Radom de o a 10
         // e mostrado ao usuário na tela
+
         btnRodarRoleta.setOnClickListener{
-            val texto = findViewById<TextView>(R.id.text_result)
-            val numeroSorteado = Random().nextInt(2)
-            val renderText = "Número sorteado é $numeroSorteado"
-            texto.text = renderText
+            val textoNumeroDigitado  = findViewById<EditText>(R.id.input_numero)
+            val textoResultado = findViewById<TextView>(R.id.text_result)
+            val textoSorteado = findViewById<TextView>(R.id.text_valor_sorteado)
+
+            val numeroSorteado = Random().nextInt(11)
+            val intNumeroDigitado = textoNumeroDigitado.text.toString().toInt()
+            val intNumeroSorteado = numeroSorteado.toString().toInt()
+
+            if (intNumeroDigitado < 0 || intNumeroDigitado > 10 ) {
+                val renderText = "Erro: Digite valores entre 0 e 10"
+                textoResultado.text = renderText
+            } else {
+                if(intNumeroDigitado >= numeroSorteado) {
+                    val renderText = "Parabens o numero digitado é maior que o sorteado"
+                    val renderTextSorteado = "Valor sorteado: $intNumeroSorteado"
+                    textoResultado.text = renderText
+                    textoSorteado.text = renderTextSorteado
+                } else {
+                    val renderText = "Infelizmente o numero digitado é menor que o sorteado"
+                    val renderTextSorteado = "Valor sorteado: $intNumeroSorteado"
+                    textoResultado.text = renderText
+                    textoSorteado.text = renderTextSorteado
+                }
+            }
         }
     }
 }
+
